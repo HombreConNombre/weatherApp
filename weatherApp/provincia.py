e2d09124d73d53bd7b19ab1801aa230737371c0b
Astrock <masterlord2100@gmail.com>
2023-07-27 11:43:47 +0200

    0.0.1 - Empezamos cagándola
    Inicio malo, tengo que dejar este proyecto aparcado por uno de CV. Seguiré los ratos libres que pueda sacar.
    Errores:
    - Quiero coger el JSON pasado por AEMET y transformarlo en objetos personalizados.
    - Json.loads() no hace lo que se supone que hace, porque se queja de lo que se le pasa... El resto va bien, aunque queda toda la estructura y todo. Estoy solo con la parte lógica y de APIs.

---
+++ weatherApp/provincia.py
@@ -0,0 +1,7 @@
+class provincia:
+    def __init__( self, codigo, nombre, codigo_autonomia, comunidad_autonoma, capital):
+        self.cod_prov = codigo
+        self.name_prov = nombre
+        self.cod_auton = codigo_autonomia
+        self.com_auton = comunidad_autonoma
+        self.capital_city = capital
--- weatherApp/weatherNow/views.py
+++ weatherApp/weatherNow/views.py
@@ -1,21 +1,55 @@
 import os
+import json
 from django.http import HttpResponse
 from django.template import loader
 import requests
 
-#Hacemos llamadas a la API de AEMET
-def cargar_provicias():
-    response = requests.get('https://www.el-tiempo.net/api/json/v2/provincias')
+path_root = os.path.dirname(__file__)
 
-    if response.status_code == 200:
-       path = os.path.join(os.path.dirname(__file__), 'dictionary.txt')
-       file = open( path, "w")
-       file.write(response.json)
-    
-    return None
+def write_log(error):
+    try:
+        file_path = "static/provincias.txt"
+        complete_path = os.path.join( path_root, file_path)
+        file = open( complete_path, "a")
+        file.write(error)
+        return None
+    except FileExistsError:
+        print("No gusta esto...")
+        return None
+#Hacemos llamadas a la API de AEMET para cargar un JSON con todos los códigos de las provincias.
+def cargar_provicias():
+    respuesta = requests.get('https://www.el-tiempo.net/api/json/v2/provincias')
+    try:
+        if respuesta.status_code == 200:
+            file_path = "static/municipios.json"
+            complete_path = os.path.join( path_root, file_path)
+            file = open( complete_path, "w")
+            #file.write(str(respuesta.content))
+            print( respuesta.json())
+        return None
+    except FileNotFoundError:
+        write_log("Archivo provincias.json no encontrado.")
+        return None
+
+#Hacemos llamadas a la API de AEMET para cargar un JSON con todos los códigos de los municipios.
+def cargar_municipios():
+    respuesta = requests.get('https://www.el-tiempo.net/api/json/v2/municipios')
+    try:
+        if respuesta.status_code == 200:
+            file_path = "static/municipios.json"
+            complete_path = os.path.join( path_root, file_path)
+            file = open( complete_path, "w")
+            file.write(str(respuesta.content))
+        
+        return None
+    except FileNotFoundError:
+        write_log("Archivo municipios.json no encontrado.")
+        return None
+
 
 # Cargamos el index de WeatherNow
 def weatherNow(request):
     html_template = loader.get_template('index.html')
     cargar_provicias()
+    cargar_municipios()
     return HttpResponse(html_template.render())
